86777ce8bb04c4fb3ccfda6974450e381250a9e5
asolovov <andre.solovov@gmail.com>
2023-02-24 13:57:58 +0400

    backend simulation

---
+++ helpers/backendECDSA.ts
@@ -0,0 +1,43 @@
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import {keccak256} from "hardhat/internal/util/keccak";
+import {ethers} from "hardhat";
+import Web3 from "web3";
+
+const web3 = new Web3();
+
+export default class BackendECDSA {
+    constructor(owner: SignerWithAddress, allowList: string[]) {
+        this.owner = owner
+
+        this.allowList = allowList
+
+        this.checkAndSign = async (user: string, amount: number) => {
+
+            // In real implementation others checks are required for example amount
+            if (this.allowList.includes(user)) {
+
+                // This hash will be compared in the contract
+                const amountBN = web3.eth.abi.encodeParameter("uint256", amount.toString());
+                const messageHash = keccak256(Buffer.concat(
+                    [
+                        Buffer.from(user.replace("0x", ""), "hex"),
+                        Buffer.from(amountBN.replace("0x", ""), "hex")
+                    ])
+                );
+
+                // This sign will be checked in the contract
+                const messageBytes = ethers.utils.arrayify(messageHash);
+                const sign = await this.owner.signMessage(messageBytes);
+
+                return {hash: messageHash, sign: sign, ok: true};
+            } else {
+                return {hash: null, sign: null, ok: false}
+            }
+        }
+    }
+
+    owner: SignerWithAddress
+    allowList: string[]
+    // @ts-ignore
+    async checkAndSign(user: string, amount: number): Promise<{ok:boolean, hash: Buffer | null, sign: any}>
+}
---
+++ helpers/backendMerkle.ts
@@ -0,0 +1,43 @@
+import {keccak256} from "hardhat/internal/util/keccak";
+import {MerkleTree} from "merkletreejs";
+import Web3 from "web3";
+
+const web3 = new Web3();
+
+export default class BackendMerkle {
+    constructor(args: {address: string, amount: number}[]) {
+        this.tree = createMerkleTree(args);
+
+        this.root = this.tree.getHexRoot();
+
+        this.getProof = (address: string, amount: number) => {
+            const amountEncode = web3.eth.abi.encodeParameter("uint256", amount);
+
+            const node = keccak256(Buffer.concat(
+                [
+                    Buffer.from(address.replace("0x", ""), "hex"),
+                    Buffer.from(amountEncode.replace("0x", ""), "hex")
+                ])
+            );
+
+            return  this.tree.getHexProof(node);
+        }
+    }
+
+    tree: MerkleTree
+    root: string
+    // @ts-ignore
+    getProof(address: string, amount: number): string[]
+}
+
+const createMerkleTree = (args: {address: string, amount: number}[]) => {
+    const leafs = args.map(arg => keccak256(Buffer.concat(
+            [
+                Buffer.from(arg.address.replace("0x", ""), "hex"),
+                Buffer.from(web3.eth.abi.encodeParameter("uint256", arg.amount).replace("0x", ""), "hex")
+            ])
+        )
+    );
+
+    return new MerkleTree(leafs, keccak256, {sortPairs: true});
+}
